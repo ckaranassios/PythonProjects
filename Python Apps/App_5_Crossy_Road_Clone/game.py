3f6456188fb789a2fd0897f269614357706b78f3
Christos Karanassios <ckaranassios@gmail.com>
2023-05-29 12:19:02 -0400

    app 5 finished

--- Python Apps/App_5_Crossy_Road_Clone/game.py
+++ Python Apps/App_5_Crossy_Road_Clone/game.py
@@ -1,4 +1,7 @@
 import pygame
+from gameObject import GameObject
+from player import Player
+from enemy import Enemy
 
 class Game:
 
@@ -8,28 +11,97 @@ class Game:
         self.white_colour = (255, 255, 255)
 
         self.game_window = pygame.display.set_mode((self.width, self.height))
-
-        background_image = pygame.image.load('assets/background.png')
-        self.background = pygame.transform.scale(background_image, (self.width, self.height))
-
-        treasure = pygame.image.load('assets/treasure.png')
-        self.treasure = pygame.transform.scale(treasure, (50, 50))
-
         self.clock = pygame.time.Clock()
+
+        self.background = GameObject(0, 0, self.width, self.height, 'assets/background.png')
+        self.treasure = GameObject(375, 50, 50, 50, 'assets/treasure.png')
+
+        self.level = 1.0
+        self.reset_map()
+
+    def reset_map(self):
+        self.player = Player(375, 700, 50, 50, 'assets/player.png', 10)
+        speed = 5 + (self.level * 2)
+
+        if self.level >= 4.0:
+            self.enemies = [
+                Enemy(0, 550, 50, 50, 'assets/enemy.png', speed),
+                Enemy(750, 400, 50, 50, 'assets/enemy.png', speed),
+                Enemy(0, 200, 50, 50, 'assets/enemy.png', speed)
+            ]
+        elif self.level >= 2:
+            self.enemies = [
+                Enemy(0, 550, 50, 50, 'assets/enemy.png', speed),
+                Enemy(750, 400, 50, 50, 'assets/enemy.png', speed)
+            ]            
+        else:
+            self.enemies = [
+                Enemy(0, 550, 50, 50, 'assets/enemy.png', speed)
+            ]    
 
     def draw_objects(self):
         self.game_window.fill(self.white_colour)
 
-        self.game_window.blit(self.background, (0, 0))
-        self.game_window.blit(self.treasure, (self.width/2.1, 25))
+        self.game_window.blit(self.background.image, (self.background.x, self.background.y))
+        self.game_window.blit(self.treasure.image, (self.treasure.x, self.treasure.y))
+        self.game_window.blit(self.player.image, (self.player.x, self.player.y))
+
+        for enemy in self.enemies:
+            self.game_window.blit(enemy.image, (enemy.x, enemy.y))
 
         pygame.display.update()
 
+    def move_objects(self, player_direction):
+        self.player.move(player_direction, self.height)
+        for enemy in self.enemies:
+            enemy.move(self.width)
+
+
+    def detect_collision(self, object_1, object_2):
+        if object_1.y > (object_2.y + object_2.height):
+            return False
+        elif (object_1.y + object_1.height) < object_2.y:
+            return False
+        
+        if object_1.x > (object_2.x + object_2.width):
+            return False
+        elif (object_1.x + object_1.width) < object_2.x:
+            return False
+        
+        return True
+        
+    def check_if_collided(self):
+        for enemy in self.enemies:
+            if self.detect_collision(self.player, enemy):
+                self.level = 1.0
+                return True
+        if self.detect_collision(self.player, self.treasure):
+            self.level += 0.5
+            return True
+        return False
+
     def run_game_loop(self):
+        player_direction = 0
+
         while True:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        player_direction = -1
+                    elif event.key == pygame.K_DOWN:
+                        player_direction = 1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        player_direction = 0
+
+            self.move_objects(player_direction)
+
             self.draw_objects()
+            
+            if self.check_if_collided():
+                self.reset_map()
+
             self.clock.tick(60)
